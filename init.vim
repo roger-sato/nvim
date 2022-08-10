0894efd1f5494a0734f8dca57705440387f2823e
nob(Nobuaki Sato) <nobuaki.sato@nulab.com>
2022-08-11 05:22:32 +0900

    change mapping

--- init.vim
+++ init.vim
@@ -1,6 +1,4 @@
 lua require('config')
 lua require('map')
 lua require('plugin')
-if !exists('g:vscode') 
-    lua require('style')
-endif
+lua require('style')
--- lua/style.lua
+++ lua/style.lua
@@ -1,4 +1,5 @@
-vim.cmd[[autocmd ColorScheme * hi CursorLine ctermfg=15 ctermbg=242 guibg=#161F25]]
-vim.cmd[[autocmd ColorScheme * hi LspDiagnosticsDefaultError guifg=#738C9C]]
+-- vim.cmd[[autocmd ColorScheme * hi CursorLine ctermfg=15 ctermbg=242 guibg=#161F25]]
+-- vim.cmd[[autocmd ColorScheme * hi LspDiagnosticsDefaultError guifg=#738C9C]]
 vim.cmd[[set termguicolors]]
 vim.cmd[[colorscheme codedark]]
+vim.cmd[[autocmd ColorScheme * hi TabLineSel guibg=#55576c]]
