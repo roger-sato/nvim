99fc31c2def29e594341ea6a882ff7cfa2bfae20
nob(Nobuaki Sato) <nobuaki.sato@nulab.com>
2023-04-07 06:48:15 +0900

    Update plugin settings and remove unused file and settins

--- lua/map-basic.lua
+++ lua/map-basic.lua
@@ -4,15 +4,6 @@ local ns = { noremap = true, silent = true }
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-map('i', 'jk', '<ESC>', ns)
--- map('t', 'jk', '<C-\\><C-n>', ns)
-
-map('i', 'jj', '<ESC>', ns)
--- map('t', 'jj', '<C-\\><C-n>', ns)
-
-map('t', '<ESC>', '<C-\\><C-n>', ns)
-map('t', '<S-Tab>', '<Esc>', ns)
-
 map('n', '+', '<C-a>', ns)
 map('n', '-', '<C-x>', ns)
 map('n', 'j', 'gj', ns)
--- lua/plugin-settings.lua
+++ lua/plugin-settings.lua
@@ -1,8 +1,22 @@
 local map = vim.api.nvim_set_keymap
 local ns = { noremap = true, silent = true }
 
--- <Easymotion Settings>
+-- [Settings]
+
+-- <Easymotion>
 vim.g.EasyMotion_smartcase = 1
 map('n','<Leader><Leader>f','<Plug>(easymotion-bd-f)',ns)
 map('n','<Leader><Leader>h','<Plug>(easymotion-linebackward)',ns)
 map('n','<Leader><Leader>l','<Plug>(easymotion-lineforward)',ns)
+map('n','<Leader><Leader>t','<Plug>(easymotion-bd-t)',ns)
+map('n','<Leader><Leader>s','<Plug>(easymotion-bd-f2)',ns)
+map('n','<Leader><Leader>/','<Plug>(easymotion-sn)',ns)
+map('n','<Leader><Leader>l','<Plug>(easymotion-lineforward)',ns)
+map('n','<Leader><Leader>j','<Plug>(easymotion-j)',ns)
+map('n','<Leader><Leader>k','<Plug>(easymotion-k)',ns)
+
+map('n','s','<Plug>(Sneak_s)',ns)
+map('n','S','<Plug>(Sneak_S)',ns)
+
+-- <Highlightedyank>
+vim.g.highlightedyank_highlight_duration = 150
